3d008d465042bf91d6874d012e121be6f26279a8
Louis Bichard <contact@louisjohnbichard.co.uk>
2020-08-07 13:19:58 +0100

    fix: update to 2.0

--- example/index.js
+++ example/index.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const process = require('process');
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
-const { startCPU } = require('@dazn/chaos-squirrel-cpu');
+const ChaosRunner = require('@dazn/chaos-squirrel-runner');
+const CPUAttack = require('@dazn/chaos-squirrel-attack-cpu');
 
 const HOSTNAME = '127.0.0.1';
 const PORT = 3000;
+const ATTACK_TIME = 3000;
 
 const printCPU = () => {
   exec(`ps -p ${process.pid} -o %cpu,%mem`, function(_, stdout) {
@@ -13,11 +15,19 @@ const printCPU = () => {
   });
 };
 
+const createRunner = ChaosRunner.configure({
+  probability: 0.5,
+  possibleAttacks: [{
+      createAttack: CPUAttack.configure({
+        runTime: ATTACK_TIME,
+        allowLoopEvery: 1500
+       })
+    }]
+});
+
 const server = http.createServer((req, res) => {
-  const attack = startCPU({
-    runTime: 3000,
-    allowLoopEvery: 1500
-  });
+  const runner = createRunner();
+  runner.start();
   console.time('request');
   setTimeout(function(){
     printCPU();
@@ -25,6 +35,10 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
+    setTimeout(() => {
+      console.log('attack stopped')
+      runner.stop();
+    }, ATTACK_TIME)
   }, 0)
 });
 
